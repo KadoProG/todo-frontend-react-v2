33137af6fc60c8a9ca8d5903934fbd0366d596db
KadoProG <splatrain@gmail.com>
2025-04-09 16:46:01 +0900

    update: 簡単なheader追加

--- src/components/AppLayout/index.tsx
+++ src/components/AppLayout/index.tsx
@@ -1,12 +1,22 @@
 import { SideBar } from '@/components/AppLayout/SideBar';
+import { useAuthContext } from '@/contexts/authContext';
 
 type Props = {
   children: React.ReactNode;
 };
 
-export const AppLayout: React.FC<Props> = ({ children }) => (
-  <div style={{ display: 'flex' }}>
-    <SideBar />
-    <div>{children}</div>
-  </div>
-);
+export const AppLayout: React.FC<Props> = ({ children }) => {
+  const { user } = useAuthContext();
+  return (
+    <div style={{ display: 'flex', minHeight: '100svh' }}>
+      <SideBar />
+
+      <div style={{ flex: 1 }}>
+        <div style={{ position: 'sticky', background: '#eee', display: 'flex' }}>
+          ようこそ、{user?.name}さん
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+};
--- src/pages/todo/TodoPage.tsx
+++ src/pages/todo/TodoPage.tsx
@@ -1,3 +1,4 @@
+import { AppLayout } from '@/components/AppLayout';
 import { Button } from '@/components/common/button/Button';
 import { DeleteButton } from '@/components/common/button/DeleteButton';
 import { Skeleton } from '@/components/common/feedback/Skeleton';
@@ -18,61 +19,66 @@ export const TodoPage: React.FC = () => {
   } = useTodoList();
 
   return (
-    <div style={{ padding: 8 }}>
-      <h1>Supabase ToDo App</h1>
-      <div style={{ display: 'flex', width: '100%' }}>
-        <TextField
-          control={control}
-          name="title"
-          placeholder="Add new todo"
-          required
-          type="text"
-          style={{ flex: 1 }}
-        />
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <Button onClick={handleAddTodo} disabled={isHandleLoading}>
-            Add
-          </Button>
+    <AppLayout>
+      <div style={{ padding: 8 }}>
+        <h1>Supabase ToDo App</h1>
+        <div style={{ display: 'flex', width: '100%' }}>
+          <TextField
+            control={control}
+            name="title"
+            placeholder="Add new todo"
+            required
+            type="text"
+            style={{ flex: 1 }}
+          />
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Button onClick={handleAddTodo} disabled={isHandleLoading}>
+              Add
+            </Button>
+          </div>
+        </div>
+        <div style={{ display: 'flex', flexFlow: 'column', gap: 8 }}>
+          {isLoading && (
+            <>
+              <Skeleton />
+              <Skeleton />
+              <Skeleton />
+              <Skeleton />
+              <Skeleton />
+            </>
+          )}
+          {todos.map((todo) => (
+            <div key={todo.id}>
+              <div style={{ display: 'flex', gap: 4 }}>
+                <Link to={`/todo/${todo.id}`}>{todo.title}</Link>-{' '}
+                {todo.isDone ? 'Complete' : 'Incomplete'}
+                <Button onClick={() => handleUpdateTodo(todo.id)} disabled={isHandleLoading}>
+                  {todo.isDone ? 'Incomplete' : 'Complete'}
+                </Button>
+                <DeleteButton
+                  onClick={() => handleDeleteTodo(todo.id)}
+                  disabled={isHandleLoading}
+                />
+              </div>
+              <div style={{ paddingLeft: 8 }}>
+                {todo.children?.map((child) => (
+                  <div key={child.id} style={{ display: 'flex', gap: 4 }}>
+                    <Link to={`/todo/${child.id}`}>{child.title}</Link>-{' '}
+                    {child.isDone ? 'Complete' : 'Incomplete'}
+                    <Button onClick={() => handleUpdateTodo(child.id)} disabled={isHandleLoading}>
+                      {child.isDone ? 'Incomplete' : 'Complete'}
+                    </Button>
+                    <DeleteButton
+                      onClick={() => handleDeleteTodo(child.id)}
+                      disabled={isHandleLoading}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-      <div style={{ display: 'flex', flexFlow: 'column', gap: 8 }}>
-        {isLoading && (
-          <>
-            <Skeleton />
-            <Skeleton />
-            <Skeleton />
-            <Skeleton />
-            <Skeleton />
-          </>
-        )}
-        {todos.map((todo) => (
-          <div key={todo.id}>
-            <div style={{ display: 'flex', gap: 4 }}>
-              <Link to={`/todo/${todo.id}`}>{todo.title}</Link>-{' '}
-              {todo.isDone ? 'Complete' : 'Incomplete'}
-              <Button onClick={() => handleUpdateTodo(todo.id)} disabled={isHandleLoading}>
-                {todo.isDone ? 'Incomplete' : 'Complete'}
-              </Button>
-              <DeleteButton onClick={() => handleDeleteTodo(todo.id)} disabled={isHandleLoading} />
-            </div>
-            <div style={{ paddingLeft: 8 }}>
-              {todo.children?.map((child) => (
-                <div key={child.id} style={{ display: 'flex', gap: 4 }}>
-                  <Link to={`/todo/${child.id}`}>{child.title}</Link>-{' '}
-                  {child.isDone ? 'Complete' : 'Incomplete'}
-                  <Button onClick={() => handleUpdateTodo(child.id)} disabled={isHandleLoading}>
-                    {child.isDone ? 'Incomplete' : 'Complete'}
-                  </Button>
-                  <DeleteButton
-                    onClick={() => handleDeleteTodo(child.id)}
-                    disabled={isHandleLoading}
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    </AppLayout>
   );
 };
