781a9d34676f64e511fc554c6648d33a12a26630
KadoProG <splatrain@gmail.com>
2025-04-21 17:29:48 +0900

    add: tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     open: true,
     host: '0.0.0.0',
