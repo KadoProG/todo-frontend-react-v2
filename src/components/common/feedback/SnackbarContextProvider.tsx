3c69b36e93d77aae819e996d9d37c5426e38572b
KadoProG <splatrain@gmail.com>
2025-05-28 06:46:04 +0900

    update: color パレットの追加と、snackbarのtailwind置き換え

--- src/components/common/feedback/SnackbarContextProvider.tsx
+++ src/components/common/feedback/SnackbarContextProvider.tsx
@@ -1,8 +1,29 @@
 import { snackbarContext } from '@/components/common/feedback/snackbarContext';
-import styles from '@/components/common/feedback/Snackbar.module.scss';
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
 
 type AlertColor = 'success' | 'error' | 'info' | 'warning';
+
+const alertColorStyles: {
+  type: AlertColor;
+  className: HTMLAttributes<HTMLElement>['className'];
+}[] = [
+  {
+    type: 'success',
+    className: 'bg-bg-success dark:bg-bg-success-dark',
+  },
+  {
+    type: 'info',
+    className: 'bg-bg-info dark:bg-bg-info-dark',
+  },
+  {
+    type: 'warning',
+    className: 'bg-bg-warning dark:bg-bg-warning-dark',
+  },
+  {
+    type: 'error',
+    className: 'bg-bg-error dark:bg-bg-error-dark',
+  },
+];
 
 interface SnackbarContextProviderProps {
   children: React.ReactNode;
@@ -13,7 +34,6 @@ export const SnackbarContextProvider: React.FC<SnackbarContextProviderProps> = (
     { message: string; type: AlertColor; disabled: boolean }[]
   >([]);
 
-  // メッセージを追加する関数
   const showSnackbar = React.useCallback((args: { message: string; type: AlertColor }) => {
     const newMessageObject = { ...args, disabled: false };
     setMessageObjects((prev) => [...prev, newMessageObject]);
@@ -26,17 +46,25 @@ export const SnackbarContextProvider: React.FC<SnackbarContextProviderProps> = (
   return (
     <snackbarContext.Provider value={{ showSnackbar }}>
       {props.children}
-      <div className={styles.snackbar} onClick={setDisabledAll}>
-        <div className={styles.snackbar__container}>
-          {messageObjects.map((v, i) => (
-            <div
-              key={i}
-              className={`${styles.snackbar__content} ${v.disabled ? styles.force : ''} ${styles[v.type]}`}
-            >
-              <p>{v.message}</p>
-              <span>×</span>
-            </div>
-          ))}
+      <div className="fixed top-0 left-0 z-1 flex w-full justify-center" onClick={setDisabledAll}>
+        <div className="flex w-full max-w-[300px] flex-col gap-2 rounded-2xl">
+          {messageObjects.map((v, i) => {
+            const alertColorStyle = alertColorStyles.find((style) => style.type === v.type);
+            return (
+              <div
+                key={i}
+                className={`m-2 flex w-full animate-snackbar-message overflow-hidden rounded-xl bg-bg-success ${v.disabled ? 'hidden' : ''} ${alertColorStyle?.className}`}
+              >
+                <p className="flex-1 p-2">{v.message}</p>
+                <span
+                  aria-label="閉じる"
+                  className="flex h-full cursor-pointer items-center justify-center bg-[rgba(255,255,255,0.2)] px-4 hover:bg-[rgba(255,255,255,0.4)]"
+                >
+                  ×
+                </span>
+              </div>
+            );
+          })}
         </div>
       </div>
     </snackbarContext.Provider>
