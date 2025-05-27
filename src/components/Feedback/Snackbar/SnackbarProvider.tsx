61e63e1e4740827da57e0809577de1136924b788
KadoProG <splatrain@gmail.com>
2025-05-28 07:01:41 +0900

    fix: CSSデグレ修正

--- src/components/Feedback/Snackbar/SnackbarProvider.tsx
+++ src/components/Feedback/Snackbar/SnackbarProvider.tsx
@@ -20,17 +20,23 @@ export const SnackbarProvider: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <SnackbarContext.Provider value={value}>
       {children}
-      <div className="fixed top-0 left-0 flex w-full justify-center" onClick={setDisabledAll}>
+      <div className="fixed top-0 left-0 z-1 flex w-full justify-center">
         <div className="flex w-full max-w-[300px] flex-col gap-2 rounded-2xl">
           {messageObjects.map((v, i) => {
             const alertColorStyle = alertColorStyles.find((style) => style.type === v.type);
             return (
               <div
                 key={`snackbar-${i}`}
-                className={`m-2 flex w-full animate-snackbar-message overflow-hidden rounded-xl bg-bg-success ${v.disabled ? 'hidden' : ''} ${alertColorStyle?.className}`}
+                className={`m-2 flex w-full animate-snackbar-message overflow-hidden rounded-xl ${v.disabled ? 'hidden' : ''} ${alertColorStyle?.className}`}
               >
-                <p>{v.message}</p>
-                <span>×</span>
+                <p className="flex-1 p-2">{v.message}</p>
+                <button
+                  aria-label="閉じる"
+                  className="flex h-full cursor-pointer items-center justify-center bg-[rgba(255,255,255,0.2)] px-4 hover:bg-[rgba(255,255,255,0.4)]"
+                  onClick={setDisabledAll}
+                >
+                  ×
+                </button>
               </div>
             );
           })}
