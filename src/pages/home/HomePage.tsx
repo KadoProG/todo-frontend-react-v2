685aece839d51d57a3e59573c2ea2b37d388d248
KadoProG <sho.kadoguchi@sun-asterisk.com>
2025-11-20 17:24:24 +0900

    feat: 新規登録画面の作成

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,10 +2,13 @@ import { Button } from '@/components/common/button/Button';
 import React from 'react';
 
 export const HomePage: React.FC = () => (
-  <div>
+  <div className="p-2">
     <h1>Home Page</h1>
     <p>ホームページです</p>
-    <Button href="/todo">ToDoリスト</Button>
-    <Button href="/login">ログイン</Button>
+    <div className="flex flex-col gap-2">
+      <Button href="/todo">ToDoリスト</Button>
+      <Button href="/login">ログイン</Button>
+      <Button href="/register">登録</Button>
+    </div>
   </div>
 );
