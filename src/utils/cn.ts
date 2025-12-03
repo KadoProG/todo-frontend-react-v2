4cdfa1fa4cc0df36c620e7146d578e02430b7217
KadoProG <sho.kadoguchi@sun-asterisk.com>
2025-12-03 17:46:54 +0900

    add: cn関数

---
+++ src/utils/cn.ts
@@ -0,0 +1,11 @@
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+/**
+ * クラス名を結合し、Tailwind CSSのクラス競合を解決するユーティリティ関数
+ * @param inputs - 結合するクラス名（文字列、配列、オブジェクトなど）
+ * @returns 結合されたクラス名の文字列
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
