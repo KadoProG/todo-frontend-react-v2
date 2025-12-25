cd640c490288e8a85aeb6c035ef81179b7139db2
KadoProG <sho.kadoguchi@sun-asterisk.com>
2025-12-25 17:57:02 +0900

    feat: svgをassetsから利用できるように

--- src/components/common/LoadingWithMessage.tsx
+++ src/components/common/LoadingWithMessage.tsx
@@ -1,5 +1,7 @@
 import styles from '@/components/common/LoadingWithMessage.module.scss';
 import React from 'react';
+import ReactIcon from '@/assets/react.svg?react';
+import ViteIcon from '@/assets/vite.svg?react';
 
 interface LoadingWithMessageProps {
   message?: string;
@@ -12,8 +14,8 @@ export const LoadingWithMessage: React.FC<LoadingWithMessageProps> = (props) => 
   <div className={styles.loadingWithMessage}>
     <div className={styles.loadingWithMessage__bottom}>
       <div className={styles.loadingWithMessage__bottom__image}>
-        <img src="/images/icons/react.svg" alt="React" />
-        <img src="/images/icons/vite.svg" alt="Vite" />
+        <ReactIcon />
+        <ViteIcon />
       </div>
       <div className={styles.loadingWithMessage__bottom__message}>
         <p>{props.message}</p>
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
   server: {
     open: true,
     host: '0.0.0.0',
   },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
